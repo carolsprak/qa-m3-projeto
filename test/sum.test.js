9600e244cb2bf2a00849d9d8709c47051bb8d4e8
Anne Caroline Rocha <carolsprak@gmail.com>
2023-04-26 01:16:00 -0300

    Organizando projeto

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,5 @@
 
-const sum = require('./sum');
+const sum = require('../src/sum');
 
 test('Verificar se a soma retorna valores positivos.', () => {
     expect(sum(2,2)).toBe(4);
@@ -11,5 +11,12 @@ test('Verificar se a soma retorna valores negativos.', () => {
 
 test('Verificar se a soma retorna valores positivos e negativos.', () => {
     expect(sum(2,-2)).toBe(0);
+});
+
+test('Verificar se a soma não retorna valores incorretos.', () => {
+    expect(sum(2,3)).not.toBe(6);
+    expect(sum(2,1)).not.toBe(2);
+    expect(sum(2,4)).not.toBe(8);
+    expect(sum(2,5)).not.toBe(10);
 });
  
